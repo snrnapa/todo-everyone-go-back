19a34fdf13ac0919ea710d4b3829f491a65d3a9d
snrnapa <shino.satoru@gmail.com>
2024-06-11 10:52:42 +0900

    IDをプライマリーキーとして設定（上書きされてしまう）

--- model/todo.go
+++ model/todo.go
@@ -7,12 +7,15 @@ import (
 )
 
 type Todo struct {
-	UserId    string    `gorm:"not null;primaryKey" json:"user_id"`
+	ID        uint      `gorm:"primaryKey"`
+	UserId    string    `gorm:"not null" json:"user_id"`
 	Title     string    `gorm:"type:varchar(100);not null" json:"title"`
 	Deadline  time.Time `gorm:"type:timestamp;" json:"deadline"`
 	Detail    string    `gorm:"type:varchar(100);" json:"detail"`
 	Completed bool      `gorm:"type:boolean;not null" json:"completed"`
-	gorm.Model
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
 type Comment struct {

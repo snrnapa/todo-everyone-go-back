c453716947b386a8ad3d794575954c6d429b40ce
snrnapa <shino.satoru@gmail.com>
2024-07-14 10:38:17 +0900

    ローカルではgolangをデバッグ起動したいため、本番用とローカル用で分けた

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 ## 参考サイト
 ### JWTでのログイン機能
 https://go.genzouw.com/2023/03/31/post-331/
+
+sudo docker compose -f docker-compose.yml -f docker-compose-prod.yml up -d
--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Docker Compose のビルド
-sudo docker-compose build
+# 開発環境では、デバッグを行いたいため、postgresqlのみを起動する。
+# sudo docker comopse up -d
 
-# Docker Compose でコンテナをバックグラウンドで起動
-sudo docker-compose up -d
+# 本番環境用
+# 本番環境では、goのインストールを行っていないため、go appの起動自体もdockerで行っているため、
+# docker-compose-prod.ymlの内容でコンテナの作成を行う必要がある。
+sudo docker-compose build
+sudo docker compose -f docker-compose.yml -f docker-compose-prod.yml up -d
